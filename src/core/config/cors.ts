663b337a1864579305e44641283df71e7a248836
Drex <femidavooo@gmail.com>
2024-10-31 14:27:25 +0100

    allowed origin

--- src/core/config/cors.ts
+++ src/core/config/cors.ts
@@ -10,7 +10,7 @@ const allowedHeaders: string[] = ['Content-Type', 'Authorization'];
 export const corsOptions: CorsOptions = {
     methods: allowedMethods,
     allowedHeaders,
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://nit-identity.vercel.app'],
     credentials: true,
     preflightContinue: true,
 };
